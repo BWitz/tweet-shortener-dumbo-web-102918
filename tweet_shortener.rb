1c794314ea1adf31723d2f7b684818805fd734f2
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-16 17:33:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,11 +32,11 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  shortened_tweet = tweet
-  if shortened_tweet.length > 140
-    shortened_tweet = word_substituter(tweet)
+  limited_tweet = tweet
+  if limited_tweet.length > 140
+    limited_tweet = word_substituter(tweet)
   end
-  shortened_tweet
+  limited_tweet
 end
 
 def shortened_tweet_truncator(tweet)
